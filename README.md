20f9353130299e38d5f5e7545a513fba62eb447d
ultradian <mph@emotrics.com>
2019-09-08 12:32:36 -0700

    fix missing ErrorHandler, README instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # basic-alexa-isp
 most basic Alexa skill code using node SDK
+
+have to go to IN-SKILL-PRODUCTS section of Alexa Dev console and create PRODUCTS
+
+create node_modules by command `npm install` from inside `\src` folder
+
+from that folder issue zip command `zip -r basicisp.zip * -x basicisp.zip`
+
+then you can upload using CLI with `aws lambda update-function-code --function-name <lambda function name> --zip-file fileb://basicisp.zip`
--- src/index.js
+++ src/index.js
@@ -7,37 +7,55 @@ const LaunchRequestHandler = {
   handle(handlerInput) {
     console.log('IN: LaunchRequestHandler');
 
-    // entitled products are obtained by request interceptor and stored in the session attributes
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    // got entitled products with request interceptor so now session attributes
+    const sessionAttributes = handlerInput.attributesManager
+        .getSessionAttributes();
     const entitledProducts = sessionAttributes.entitledProducts;
 
     if (entitledProducts && entitledProducts.length > 0) {
       // Customer owns one or more products
       return handlerInput.responseBuilder
-        .speak(`Welcome. You have ${entitledProducts.map(item => item.name).join(', ')}`)
-        .getResponse();
+          .speak(`Welcome. You have ${entitledProducts
+              .map((item) => item.name).join(', ')}`)
+          .getResponse();
     }
 
     // No entitled products
     console.log('No entitledProducts');
     return handlerInput.responseBuilder
-      .speak('You have no products.')
-      .getResponse();
+        .speak('You have no products.')
+        .getResponse();
   },
 }; // End LaunchRequestHandler
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${JSON.stringify(error.message)}`);
+    console.log(`handlerInput: ${JSON.stringify(handlerInput)}`);
+    return handlerInput.responseBuilder
+        .speak('Sorry, I didn\'t understand what you meant. Please try again.')
+        .reprompt('Please try again.')
+        .getResponse();
+  },
+};
 
 const SessionEndedHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
     return request.type === 'SessionEndedRequest' ||
-      (request.type === 'IntentRequest' && request.intent.name === 'AMAZON.StopIntent') ||
-      (request.type === 'IntentRequest' && request.intent.name === 'AMAZON.CancelIntent');
+      (request.type === 'IntentRequest' &&
+          request.intent.name === 'AMAZON.StopIntent') ||
+      (request.type === 'IntentRequest' &&
+          request.intent.name === 'AMAZON.CancelIntent');
   },
   handle(handlerInput) {
     console.log('IN: SessionEndedHandler.handle');
     return handlerInput.responseBuilder
-      .speak(getRandomGoodbye())
-      .getResponse();
+        .speak(getRandomGoodbye())
+        .getResponse();
   },
 };
 
@@ -47,10 +65,12 @@ const EntitledProductsCheck = {
       // new session, check to see what products are already owned.
       try {
         const locale = handlerInput.requestEnvelope.request.locale;
-        const ms = handlerInput.serviceClientFactory.getMonetizationServiceClient();
+        const ms = handlerInput.serviceClientFactory
+            .getMonetizationServiceClient();
         const result = await ms.getInSkillProducts(locale);
         const entitledProducts = getAllEntitledProducts(result.inSkillProducts);
-        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        const sessionAttributes = handlerInput.attributesManager
+            .getSessionAttributes();
         sessionAttributes.entitledProducts = entitledProducts;
         handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
       } catch (error) {
@@ -61,12 +81,12 @@ const EntitledProductsCheck = {
 };
 
 exports.handler = Alexa.SkillBuilders.standard()
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    SessionEndedHandler,
-  )
-  .addRequestInterceptors(
-    EntitledProductsCheck,
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        SessionEndedHandler,
+    )
+    .addRequestInterceptors(
+        EntitledProductsCheck,
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
